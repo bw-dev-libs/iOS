500049552e63c9f76aba612d4227358ffba41061
ladybeitel <ciarabeitel@gmail.com>
2019-09-24 12:14:25 -0400

    Build interface, create segues, add launchscreen

---
+++ DevLibs/Custom Views/DashboardTableView.swift
@@ -0,0 +1,21 @@
+//
+//  DashboardTableView.swift
+//  DevLibs
+//
+//  Created by Ciara Beitel on 9/24/19.
+//  Copyright © 2019 Ciara Beitel. All rights reserved.
+//
+
+import UIKit
+
+class DashboardTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ DevLibs/Custom Views/DashboardTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DashboardTableViewCell.swift
+//  DevLibs
+//
+//  Created by Ciara Beitel on 9/24/19.
+//  Copyright © 2019 Ciara Beitel. All rights reserved.
+//
+
+import UIKit
+
+class DashboardTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
