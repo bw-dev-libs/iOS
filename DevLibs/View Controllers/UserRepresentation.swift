229c7f8713969424d198fef68865cc75ba42bc93
Marc Jacques <Marcjacques@me.com>
2019-09-24 20:34:10 -0400

    Figuring out data model

---
+++ DevLibs/View Controllers/UserRepresentation.swift
@@ -0,0 +1,15 @@
+//
+//  UserRepresentation.swift
+//  DevLibs
+//
+//  Created by Marc Jacques on 9/24/19.
+//  Copyright © 2019 Ciara Beitel. All rights reserved.
+//
+
+import Foundation
+
+struct UserRepresentation: Codable {
+    var username: String?
+    var password: String?
+    
+}
