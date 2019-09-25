ef21a230f5d6663162dc276a70f6946b75da0809
Marc Jacques <Marcjacques@me.com>
2019-09-25 14:49:30 -0400

    Add Storyboards for additional POS and update GameViewController

--- DevLibs/View Controllers/GameViewController.swift
+++ DevLibs/View Controllers/GameViewController.swift
@@ -7,7 +7,7 @@
 //
 
 enum GameState: String {
-    case noun = "noun", verb, adjective, title, detail
+    case programmingLanguage = "programmingLanguage", noun, verb, ingVerb, edVerb, noun2, title, detail
 }
 
 import UIKit
@@ -19,13 +19,16 @@ class GameViewController: UIViewController {
     let toStoryView1 = "SegueToStoryView1"
     let toStoryView2 = "SegueToStoryView2"
     let toStoryView3 = "SegueToStoryView3"
+    let toStoryView4 = "SegueToStoryView4"
+    let toStoryView5 = "SegueToStoryView5"
+    let toStoryView6 = "SegueToStoryView6"
     let toStoryViewTitle = "SegueToStoryViewTitle"
     let toStoryViewDetail = "SegueToStoryViewDetail"
     
     var wordController: WordController?
-
+    
     var currentState: GameState = .noun
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -39,20 +42,29 @@ class GameViewController: UIViewController {
             #warning("save to core data")
             
             switch currentState {
-                case .noun:
-                    currentState = .verb
-                    self.performSegue(withIdentifier: toStoryView2, sender: self)
-                case .verb:
-                    currentState = .adjective
-                    self.performSegue(withIdentifier: toStoryView3, sender: self)
-                case .adjective:
-                    currentState = .title
-                    self.performSegue(withIdentifier: toStoryViewTitle, sender: self)
-                case .title:
-                    currentState = .detail
-                    self.performSegue(withIdentifier: toStoryViewDetail, sender: self)
-                default:
-                    return
+            case .programmingLanguage:
+                currentState = .noun
+                self.performSegue(withIdentifier: toStoryView2, sender: self)
+            case .noun:
+                currentState = .verb
+                self.performSegue(withIdentifier: toStoryView3, sender: self)
+            case .verb:
+                currentState = .ingVerb
+                self.performSegue(withIdentifier: toStoryView4, sender: self)
+            case .ingVerb:
+                currentState = .edVerb
+                self.performSegue(withIdentifier: toStoryView5, sender: self)
+            case .edVerb:
+                currentState = .noun2
+                self.performSegue(withIdentifier: toStoryView6, sender: self)
+            case .noun2:
+                currentState = .title
+                self.performSegue(withIdentifier: toStoryViewTitle, sender: self)
+            case .title:
+                currentState = .detail
+                self.performSegue(withIdentifier: toStoryViewDetail, sender: self)
+            default:
+                return
             }
         } else {
             //wordController.addWords(" ")
@@ -61,18 +73,33 @@ class GameViewController: UIViewController {
             #warning("save to core data")
         }
     }
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let wordController = wordController else { return }
         if segue.identifier == toStoryView2 {
             guard let destination = segue.destination as? GameViewController else { return }
             destination.wordController = wordController
-            destination.currentState = .verb
+            destination.currentState = .noun
         }
         if segue.identifier == toStoryView3 {
             guard let destination = segue.destination as? GameViewController else { return }
             destination.wordController = wordController
-            destination.currentState = .adjective
+            destination.currentState = .verb
+        }
+        if segue.identifier == toStoryView4 {
+            guard let destination = segue.destination as? GameViewController else { return }
+            destination.wordController = wordController
+            destination.currentState = .ingVerb
+        }
+        if segue.identifier == toStoryView5 {
+            guard let destination = segue.destination as? GameViewController else { return }
+            destination.wordController = wordController
+            destination.currentState = .edVerb
+        }
+        if segue.identifier == toStoryView6 {
+            guard let destination = segue.destination as? GameViewController else { return }
+            destination.wordController = wordController
+            destination.currentState = .noun2
         }
         if segue.identifier == toStoryViewTitle {
             guard let destination = segue.destination as? GameViewController else { return }
